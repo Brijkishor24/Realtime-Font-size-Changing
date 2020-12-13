d1035b474527745aed164587cf49fbdd5cb2d112
Brijkishor24 <71825478+Brijkishor24@users.noreply.github.com>
2020-12-13 10:29:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
+noseX=0;
+noseY=0;
 function setup(){
-    canvas=createCanvas(600,400);
-    canvas.position(50,200);
+    canvas=createCanvas(600,490);
+    canvas.position(650,200);
     video=createCapture(VIDEO);
-    video.hide();
+    video.size(600,400);
+    video.position(50,250);
+
+    posenet=ml5.poseNet(video,modelloaded);
+    posenet.on('pose',getPoses);
+}
+function modelloaded(){
+    console.log("PoseNet Initialized");
+}
+function getPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+    }
 }
 function draw(){
-    image(video,0,0,600,400);
+    background("lightgreen");
 }
